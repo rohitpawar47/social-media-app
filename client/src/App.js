37cf1ff3c3a04bf2d34cc87625ab53ca0b2471c6
rohitpawar47 <rohitpawar.skills@gmail.com>
2023-05-27 12:29:00 +0530

    intial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './Reset.css';
+import styled from 'styled-components';
+
+const Headline = styled.h1`
+  color: red;
+  text-align: center;
+`;
+
+function App() {
+  return (
+    <div>
+      <Headline>Client Side</Headline>
+    </div>
+  );
+}
+
+export default App;
